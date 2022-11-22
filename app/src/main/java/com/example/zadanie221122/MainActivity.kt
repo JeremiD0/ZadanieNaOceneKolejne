9f3bc3bd927d846d7aaacc88cef0853dff76943d
Majster na beacie ziomal. Latam na dronie jak na tureckim dywanie. Pytasz czy jaram? <dilerdanych@kxh-research.org>
2022-11-22 10:04:15 +0100

    komentarz brak

--- app/src/main/java/com/example/zadanie221122/MainActivity.kt
+++ app/src/main/java/com/example/zadanie221122/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.zadanie221122
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val s1 = findViewById<Switch>(R.id.switch1).text
+        
     }
 }
